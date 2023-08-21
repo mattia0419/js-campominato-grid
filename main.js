8a5eae2c8c5f38413de626f0d008cf1ad8bf6f3f
mattia0419 <pironemattia6@gmail.com>
2023-08-21 16:55:39 +0200

    add other difficult

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 const buttonGenerator = document.getElementById("generator");
 
+const buttonGenerator2 = document.getElementById("generator-2");
+
+const buttonGenerator3 = document.getElementById("generator-3");
+
+
 const grid = document.getElementById("griglia");
 
 buttonGenerator.addEventListener("click", function(){
-    generaGriglia();
+    generaGriglia(10);
+})
+
+buttonGenerator2.addEventListener("click", function(){
+    generaGriglia(9);
+})
+
+buttonGenerator3.addEventListener("click", function(){
+    generaGriglia(7);
 })
 
 
 
-function generaGriglia(){
-    for(let i = 1; i <= 100; i++){
+function generaGriglia(size){
+    for(let i = 1; i <= size * size; i++){
         const cell = document.createElement("div");
-        cell.classList.add("cell");
+        
+        if(size == 10){
+            cell.classList.add("cell");
+        }
+        else if(size == 9){
+            cell.classList.add("cell-9");
+        }
+        else if(size == 7){
+            cell.classList.add("cell-7");
+        }
+
+        
         cell.innerText = i;
         cell.addEventListener("click", function(){
             this.classList.add("azure");
